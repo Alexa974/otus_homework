7c16ddd037460815523fb21b3c957576ff95c502
alex <yposs@inbox.ru>
2023-04-20 22:45:36 +0300

    add Bootstrap in templates

--- homework_07/inventory/inventory/settings.py
+++ homework_07/inventory/inventory/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'inventory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['mainapp/templates/mainapp'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
